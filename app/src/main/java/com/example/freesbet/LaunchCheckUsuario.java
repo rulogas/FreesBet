ce90330d4e97d39ea82546bec3bec7cf19731d8a
rulogas <rulogas@hotmail.es>
2019-05-14 00:02:30 +0200

    logo aplicacion y splash screen

--- app/src/main/java/com/example/freesbet/LaunchCheckUsuario.java
+++ app/src/main/java/com/example/freesbet/LaunchCheckUsuario.java
@@ -4,6 +4,7 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,36 +16,29 @@ import java.io.File;
 
 public class LaunchCheckUsuario extends AppCompatActivity {
 
-    ProgressDialog nDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch_check_usuario);
-        iniciarLoader();
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Intent in = new Intent(LaunchCheckUsuario.this,Home.class);
+                    startActivity(in);
+                } else {
+                    // No user is signed in
+                    Intent in = new Intent(LaunchCheckUsuario.this,FirebaseUIActivity.class);
+                    startActivity(in);
+                }
+                finish();
+            }
+        }, 1000);
 
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        if (user != null) {
-            // User is signed in
-            Intent in = new Intent(LaunchCheckUsuario.this,Home.class);
-            nDialog.dismiss();
-            startActivity(in);
-        } else {
-            // No user is signed in
-            Intent in = new Intent(LaunchCheckUsuario.this,FirebaseUIActivity.class);
-            nDialog.dismiss();
-            startActivity(in);
-        }
-        finish();
-    }
-
-    private void iniciarLoader(){
-        nDialog = new ProgressDialog(LaunchCheckUsuario.this);
-        nDialog.setMessage("Cargando..");
-        nDialog.setTitle("Obteniendo datos");
-        nDialog.setIndeterminate(false);
-        nDialog.setCancelable(true);
-        nDialog.show();
     }
 
 }
